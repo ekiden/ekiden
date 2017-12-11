8d12b0723a4fbea99e8e1e07f4a52c8352a4158f
Fan Zhang <fanz@linux.com>
2017-12-10 23:14:09 -0500

    dummy client & server impl

---
+++ client/src/bin/ekiden_client.rs
@@ -0,0 +1,21 @@
+extern crate ekiden_web3;
+extern crate grpc;
+extern crate futures;
+
+use ekiden_web3::ekiden_web3_grpc::*;
+use ekiden_web3::ekiden_web3::*;
+
+use std::env;
+
+fn main() {
+    let name = env::args().nth(1).map(|s| s.to_owned()).unwrap_or_else(|| "world".to_owned());
+
+    let client = EkidenClient::new_plain("localhost", 9001, Default::default()).unwrap();
+
+    let mut req = HelloRequest::new();
+    req.set_name(name);
+
+    let resp = client.say_hello(grpc::RequestOptions::new(), req);
+
+    println!("{:?}", resp.wait());
+}
---
+++ client/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate futures;
+extern crate futures_cpupool;
+extern crate protobuf;
+extern crate grpc;
+extern crate tls_api;
+
+pub mod ekiden_web3;
+pub mod ekiden_web3_grpc;
