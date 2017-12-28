9b5fdb3506ba16c66155c989231eb3e3e5cc68a8
Raymond Cheng <me@raymondcheng.net>
2017-12-28 14:19:18 +0800

    adding library to token contract

---
+++ contracts/token/src/bin/main.rs
@@ -0,0 +1,5 @@
+
+fn main() {
+  println!("Token contract starting... ");
+
+}
--- contracts/token/src/lib.rs
+++ contracts/token/src/lib.rs
@@ -4,7 +4,3 @@ extern crate protobuf;
 mod token_contract;
 mod generated;
 
-fn main() {
-  println!("Token contract starting... ");
-
-}
