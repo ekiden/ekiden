c6537927862c56762848d07579004de454a1bcb4
Jernej Kos <jernej@kos.mx>
2017-12-30 22:40:49 +0100

    Add macro to simplify enclave creation

--- enclaves/dummy/src/lib.rs
+++ enclaves/dummy/src/lib.rs
@@ -2,21 +2,14 @@
 
 #![no_std]
 
-#[macro_use]
 extern crate sgx_tstd as std;
 
+#[macro_use]
 extern crate libenclave_trusted;
 
 #[allow(unused)]
 #[prelude_import]
 use std::prelude::v1::*;
 
-#[no_mangle]
-pub extern "C" fn rpc_call(request_data: *const u8,
-                           request_length: usize,
-                           response_data: *mut u8,
-                           response_capacity: usize,
-                           response_length: *mut usize) {
-    // TODO: Find a way to forward this automatically.
-    libenclave_trusted::call(request_data, request_length, response_data, response_capacity, response_length);
-}
+// Create enclave glue.
+create_enclave!();
--- libenclave/trusted/src/lib.rs
+++ libenclave/trusted/src/lib.rs
@@ -14,6 +14,43 @@ use std::prelude::v1::*;
 
 mod rpc;
 
+/// Emits all needed code for enclave glue.
+///
+/// This macro should be used to create any enclave glue that is needed for
+/// the Ekiden enclaves to function correctly.
+///
+/// A minimal enclave is as follows:
+/// ```
+/// #![feature(prelude_import)]
+///
+/// #![no_std]
+///
+/// #[macro_use]
+/// extern crate sgx_tstd as std;
+///
+/// #[macro_use]
+/// extern crate libenclave_trusted;
+///
+/// #[allow(unused)]
+/// #[prelude_import]
+/// use std::prelude::v1::*;
+///
+/// create_enclave!();
+/// ```
+#[macro_export]
+macro_rules! create_enclave {
+    () => {
+        #[no_mangle]
+        pub extern "C" fn rpc_call(request_data: *const u8,
+                                   request_length: usize,
+                                   response_data: *mut u8,
+                                   response_capacity: usize,
+                                   response_length: *mut usize) {
+            libenclave_trusted::call(request_data, request_length, response_data, response_capacity, response_length);
+        }
+    }
+}
+
 /// TODO: Documentation.
 pub fn call(request_data: *const u8,
             request_length: usize,
