776879050a71d5284ec9a3e7bf46f19a1589269d
Raymond Cheng <me@raymondcheng.net>
2018-01-04 17:19:23 -0800

    adding grpc server to storage

--- storage/src/main.rs
+++ storage/src/main.rs
@@ -1,4 +1,5 @@
 extern crate abci;
+extern crate grpc;
 extern crate tokio_proto;
 
 mod ekidenmint;
@@ -9,14 +10,19 @@ use tokio_proto::TcpServer;
 use abci::server::{ AbciProto, AbciService };
 
 fn main() {
-  //let args: Vec<String> = env::args().collect();
-  //let connection_type: &str = &args[1];
-  //let listen_addr: &str = &args[2];
-  let listen_addr = "127.0.0.1:46658".parse().unwrap();
-
   println!("Ekiden Storage starting... ");
+  // Start the gRPC server.
+  let port = 9002;
+  let mut server = grpc::ServerBuilder::new_plain();
+  server.http.set_port(port);
+  server.http.set_cpu_pool_threads(1);
+  //server.add_service(ComputeServer::new_service_def(ComputeServerImpl::new(contract)));
+  let _server = server.build().expect("server");
+
+  // Start the ABCI listener
+  let abci_listen_addr = "127.0.0.1:46658".parse().unwrap();
   let app = ekidenmint::Ekidenmint::new();
-  let app_server = TcpServer::new(AbciProto, listen_addr);
+  let app_server = TcpServer::new(AbciProto, abci_listen_addr);
   app_server.serve(move || {
     Ok(AbciService {
       app: Box::new(app.clone()),
