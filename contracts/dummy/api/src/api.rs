4713df84e68b22b7c42aa21ebb9334ee051c0e1b
w <none>
2018-01-10 16:31:03 -0800

    dummy: use wkt Empty

--- contracts/dummy/api/src/api.rs
+++ contracts/dummy/api/src/api.rs
@@ -2,7 +2,7 @@ contract_api! {
     metadata {
         name = dummy;
         version = "0.1.0";
-        state_type = libcontract_common::api::Void;
+        state_type = protobuf::well_known_types::Empty;
     }
 
     rpc hello_world(HelloWorldRequest) -> HelloWorldResponse;
--- contracts/dummy/src/lib.rs
+++ contracts/dummy/src/lib.rs
@@ -18,17 +18,17 @@ extern crate protobuf;
 use std::prelude::v1::*;
 
 use dummy_api::{HelloWorldRequest, HelloWorldResponse};
+use protobuf::well_known_types::Empty;
 
 use libcontract_common::ContractError;
-use libcontract_common::api::Void;
 
 create_enclave_api!();
 
-fn hello_world(_state: Void, request: HelloWorldRequest) -> Result<(Void, HelloWorldResponse), ContractError> {
+fn hello_world(_state: Empty, request: HelloWorldRequest) -> Result<(Empty, HelloWorldResponse), ContractError> {
     println!("hello world called");
 
     let mut response = HelloWorldResponse::new();
     response.set_world(format!("enclave says {}", request.hello));
 
-    Ok((Void::new(), response))
+    Ok((Empty::new(), response))
 }
