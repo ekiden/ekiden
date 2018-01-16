444e9df9a7683baa51057cbba9dae37c4877f754
Nick Hynes <nhynes@berkeley.edu>
2018-01-16 05:36:39 -0800

    Create utils for client binaries

--- clients/token/src/main.rs
+++ clients/token/src/main.rs
@@ -1,51 +1,16 @@
-#[macro_use]
-extern crate compute_client;
+#[macro_use] extern crate clap;
 
-#[macro_use]
-extern crate token_api;
+#[macro_use] extern crate compute_client;
+#[macro_use] extern crate client_utils;
 
-#[macro_use]
-extern crate clap;
+#[macro_use] extern crate token_api;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 create_client_api!();
 
 fn main() {
-    let matches = App::new("Ekiden Token Contract Client")
-                      .version("0.1.0")
-                      .author("Jernej Kos <jernej@kos.mx>")
-                      .about("Client for the Ekiden Token Contract")
-                      .arg(Arg::with_name("host")
-                           .long("host")
-                           .short("h")
-                           .takes_value(true)
-                           .default_value("localhost")
-                           .display_order(1))
-                      .arg(Arg::with_name("port")
-                           .long("port")
-                           .short("p")
-                           .takes_value(true)
-                           .default_value("9001")
-                           .display_order(2))
-                      .arg(Arg::with_name("mr-enclave")
-                           .long("mr-enclave")
-                           .value_name("MRENCLAVE")
-                           .help("MRENCLAVE in hex format")
-                           .takes_value(true)
-                           .required(true)
-                           .display_order(3))
-                      .get_matches();
-
-    let backend = compute_client::backend::Web3ContractClientBackend::new(
-        matches.value_of("host").unwrap(),
-        value_t!(matches, "port", u16).unwrap_or(9001)
-    ).unwrap();
-
-    let mut client = token::Client::new(
-        backend,
-        value_t!(matches, "mr-enclave", compute_client::MrEnclave).unwrap_or_else(|e| e.exit())
-    ).unwrap();
+    let mut client = contract_client!(token);
 
     // Create new token contract.
     let mut request = token::CreateRequest::new();
