119de032ff513a1704d7477afd0be2034f6cc2bb
w <none>
2018-01-23 14:56:24 -0800

    compute: wip batching server. compiles

--- compute/src/server.rs
+++ compute/src/server.rs
@@ -2,7 +2,6 @@ use grpc;
 use protobuf;
 use protobuf::Message;
 use std;
-use thread_local::ThreadLocal;
 
 use std::fmt::Write;
 
@@ -19,11 +18,15 @@ use generated::storage_grpc::Storage;
 
 use super::ias::{IASConfiguration, IAS};
 
+struct QueuedCall {
+    rpc_request: CallContractRequest,
+    grpc_response: grpc::SingleResponse<CallContractResponse>,
+    response_sender: std::sync::mpsc::SyncSender<grpc::SingleResponse<CallContractResponse>>,
+}
+
 pub struct ComputeServerImpl {
-    // Filename of the enclave implementing the contract.
-    contract_filename: String,
-    // Contract running in an enclave.
-    contract: ThreadLocal<enclave::EkidenEnclave>,
+    // Channel for submitting requests to the worker.
+    request_sender: std::sync::Mutex<std::sync::mpsc::Sender<QueuedCall>>,
     // IAS service.
     ias: IAS,
 }
@@ -31,88 +34,144 @@ pub struct ComputeServerImpl {
 impl ComputeServerImpl {
     /// Create new compute server instance.
     pub fn new(contract_filename: &str, ias: IASConfiguration) -> Self {
+        let contract = Self::get_contract(&contract_filename);
+        let (request_sender, request_receiver) = std::sync::mpsc::channel();
+        std::thread::spawn(move || { // contract, request_receiver
+            // Block for the next call.
+            // When ComputeServerImpl is dropped, the request_sender closes, and the thread will exit.
+            while let Ok(queued_call) = request_receiver.recv() {
+                let mut call_batch = Vec::new();
+                call_batch.push(queued_call);
+                // Additionally dequeue any remaining requests.
+                while let Ok(queued_call) = request_receiver.try_recv() {
+                    call_batch.push(queued_call);
+                }
+                // Process the requests.
+                Self::call_contract_batch(&contract, call_batch);
+            }
+        });
         ComputeServerImpl {
-            contract_filename: contract_filename.to_string(),
-            contract: ThreadLocal::new(),
+            request_sender: std::sync::Mutex::new(request_sender),
             ias: IAS::new(ias).unwrap(),
         }
     }
 
     /// Get thread-local instance of the contract.
-    fn get_contract(&self) -> &enclave::EkidenEnclave {
-        self.contract.get_or(|| {
-            // TODO: Handle contract initialization errors.
-            let contract = enclave::EkidenEnclave::new(&self.contract_filename).unwrap();
+    fn get_contract(contract_filename: &str) -> enclave::EkidenEnclave {
+        // TODO: Handle contract initialization errors.
+        let contract = enclave::EkidenEnclave::new(contract_filename).unwrap();
 
-            // Initialize contract.
-            // TODO: Support contract restore.
-            let response = contract
-                .initialize()
-                .expect("Failed to initialize contract");
+        // Initialize contract.
+        // TODO: Support contract restore.
+        let response = contract
+            .initialize()
+            .expect("Failed to initialize contract");
 
-            // Show contract MRENCLAVE in hex format.
-            let mut mr_enclave = String::new();
-            for &byte in response.get_mr_enclave() {
-                write!(&mut mr_enclave, "{:02x}", byte).unwrap();
-            }
+        // Show contract MRENCLAVE in hex format.
+        let mut mr_enclave = String::new();
+        for &byte in response.get_mr_enclave() {
+            write!(&mut mr_enclave, "{:02x}", byte).unwrap();
+        }
 
-            println!("Loaded contract with MRENCLAVE: {}", mr_enclave);
+        println!("Loaded contract with MRENCLAVE: {}", mr_enclave);
 
-            Box::new(contract)
-        })
+        contract
     }
 
-    fn call_contract_fallible(
-        &self,
-        rpc_request: CallContractRequest,
-    ) -> Result<CallContractResponse, Box<std::error::Error>> {
-        // Connect to storage node
-        // TODO: Let client select storage node.
+    fn call_contract_fallible(contract: &enclave::EkidenEnclave, encrypted_state_opt: Option<libcontract_common::api::CryptoSecretbox>, rpc_request: &CallContractRequest) -> Result<(Option<libcontract_common::api::CryptoSecretbox>, CallContractResponse), Box<std::error::Error>> {
+        let mut enclave_request = libcontract_common::api::EnclaveRequest::new();
+        let client_request = protobuf::parse_from_bytes(rpc_request.get_payload())?;
+        enclave_request.set_client_request(client_request);
+        if let Some(encrypted_state) = encrypted_state_opt {
+            enclave_request.set_encrypted_state(encrypted_state);
+        }
+
+        let enclave_request_bytes = enclave_request.write_to_bytes()?;
+        let enclave_response_bytes = contract.call_raw(&enclave_request_bytes)?;
+
+        let mut enclave_response: libcontract_common::api::EnclaveResponse =
+            protobuf::parse_from_bytes(&enclave_response_bytes)?;
+        let mut rpc_response = CallContractResponse::new();
+        rpc_response.set_payload(enclave_response.get_client_response().write_to_bytes()?);
+        let new_encrypted_state_opt = if enclave_response.has_encrypted_state() {
+            Some(enclave_response.take_encrypted_state())
+        } else {
+            None
+        };
+
+        Ok((new_encrypted_state_opt, rpc_response))
+    }
+
+    fn call_contract_batch_fallible(
+        contract: &enclave::EkidenEnclave,
+        call_batch: &mut [QueuedCall],
+    ) -> Result<(), Box<std::error::Error>> {
+        // Connect to storage node.
+        // TODO: Know the storage node location other than having it hard-coded.
         // TODO: Use TLS client.
         let storage_client =
             storage_grpc::StorageClient::new_plain("localhost", 9002, Default::default())?;
 
-        //
-        let mut enclave_request = libcontract_common::api::EnclaveRequest::new();
-
-        // Get state from storage
-        let storage_result = storage_client
+        // Get state from consensus.
+        let consensus_result = storage_client
             .get(grpc::RequestOptions::new(), storage::GetRequest::new())
             .wait();
-        if let Ok((_, storage_get_response, _)) = storage_result {
+        let mut encrypted_state_opt = if let Ok((_, storage_get_response, _)) = consensus_result {
             let encrypted_state = protobuf::parse_from_bytes(storage_get_response.get_payload())?;
-            enclave_request.set_encrypted_state(encrypted_state);
+            Some(encrypted_state)
+        } else {
+            // We should bail if there was an error other than the storage not being initialized.
+            // But don't go fixing this. There's another resolution planned in #95.
+            None
         };
-        // We should bail if there was an error other than the storage not being initialized.
-        // But don't go fixing this. There's another resolution planned in #95.
 
-        //
-        let client_request: libcontract_common::api::ClientRequest =
-            protobuf::parse_from_bytes(rpc_request.get_payload())?;
-        enclave_request.set_client_request(client_request);
-
-        let enclave_request_bytes = enclave_request.write_to_bytes()?;
-        let enclave_response_bytes = self.get_contract().call_raw(&enclave_request_bytes)?;
-
-        let enclave_response: libcontract_common::api::EnclaveResponse =
-            protobuf::parse_from_bytes(&enclave_response_bytes)?;
+        // Process the requests.
+        for ref mut queued_call in call_batch {
+            queued_call.grpc_response = match Self::call_contract_fallible(contract, encrypted_state_opt.clone(), &queued_call.rpc_request) {
+                Ok((new_encrypted_state_opt, rpc_response)) => {
+                    if let Some(new_encrypted_state) = new_encrypted_state_opt {
+                        encrypted_state_opt = Some(new_encrypted_state);
+                    }
+                    grpc::SingleResponse::completed(rpc_response)
+                },
+                Err(e) => {
+                    grpc::SingleResponse::err(grpc::Error::Panic(String::from(e.description())))
+                },
+            };
+        }
 
         // Set state in storage
-        if enclave_response.has_encrypted_state() {
-            let new_encrypted_state = enclave_response.get_encrypted_state();
+        if let Some(encrypted_state) = encrypted_state_opt {
             let mut storage_set_request = storage::SetRequest::new();
-            storage_set_request.set_payload(new_encrypted_state.write_to_bytes()?);
+            storage_set_request.set_payload(encrypted_state.write_to_bytes()?);
             storage_client
                 .set(grpc::RequestOptions::new(), storage_set_request)
                 .wait()?;
         }
 
-        //
-        let client_response = enclave_response.get_client_response();
+        Ok(())
+    }
 
-        let mut rpc_response = CallContractResponse::new();
-        rpc_response.set_payload(client_response.write_to_bytes()?);
-        Ok(rpc_response)
+    fn call_contract_batch(
+        contract: &enclave::EkidenEnclave,
+        mut call_batch: Vec<QueuedCall>,
+    ) {
+        match Self::call_contract_batch_fallible(contract, &mut call_batch) {
+            Ok(_) => {
+                // No batch-wide errors. Successful calls can go out.
+                for queued_call in call_batch {
+                    queued_call.response_sender.send(queued_call.grpc_response).unwrap();
+                }
+            },
+            Err(e) => {
+                // Send batch-wide error to all clients.
+                let desc = String::from(e.description());
+                for queued_call in call_batch {
+                    let grpc_response = grpc::SingleResponse::err(grpc::Error::Panic(desc.clone()));
+                    queued_call.response_sender.send(grpc_response).unwrap();
+                }
+            },
+        }
     }
 }
 
@@ -122,12 +181,12 @@ impl Compute for ComputeServerImpl {
         _options: grpc::RequestOptions,
         rpc_request: CallContractRequest,
     ) -> grpc::SingleResponse<CallContractResponse> {
-        match self.call_contract_fallible(rpc_request) {
-            Ok(rpc_response) => grpc::SingleResponse::completed(rpc_response),
-            Err(_) => {
-                return grpc::SingleResponse::err(grpc::Error::Other("Failed to call contract"))
-            }
+        let (response_sender, response_receiver) = std::sync::mpsc::sync_channel(0);
+        {
+            let request_sender = self.request_sender.lock().unwrap();
+            request_sender.send(QueuedCall { rpc_request, grpc_response: grpc::SingleResponse::err(grpc::Error::Other("Call did not run")), response_sender }).unwrap();
         }
+        response_receiver.recv().unwrap()
     }
 
     fn ias_get_spid(
