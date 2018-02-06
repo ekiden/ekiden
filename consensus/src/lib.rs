92aa32e55cfea5add216f2da2eb3cad5a9b0de02
Raymond Cheng <me@raymondcheng.net>
2018-02-06 13:06:31 -0800

    moving code to integration test

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -68,70 +68,9 @@ pub fn run(config: &Config) -> Result<(), Box<Error>> {
 #[cfg(test)]
 mod tests {
     use super::generated::consensus;
-    use super::generated::consensus_grpc;
-    use super::generated::consensus_grpc::Consensus;
-    use grpc;
 
     #[test]
-    fn exercise1() {
-        //let consensus_client =
-        //    consensus_grpc::ConsensusClient::new_plain("localhost", 9002, Default::default())
-        //        .unwrap();
-
-        //// Set state to `helloworld`
-        //let mut consensus_set_request = consensus::SetRequest::new();
-        //consensus_set_request.set_payload(String::from("helloworld").into_bytes());
-        //consensus_client
-        //    .set(grpc::RequestOptions::new(), consensus_set_request)
-        //    .wait()
-        //    .unwrap();
-
-        //let consensus_get_request = consensus::GetRequest::new();
-        //let (_, consensus_get_response, _) = consensus_client
-        //    .get(grpc::RequestOptions::new(), consensus_get_request)
-        //    .wait()
-        //    .unwrap();
-        //assert_eq!(
-        //    consensus_get_response.get_payload(),
-        //    String::from("helloworld").as_bytes()
-        //);
-
-        //// Set state to `successor`
-        //let mut consensus_set_request = consensus::SetRequest::new();
-        //consensus_set_request.set_payload(String::from("successor").into_bytes());
-        //consensus_client
-        //    .set(grpc::RequestOptions::new(), consensus_set_request)
-        //    .wait()
-        //    .unwrap();
-
-        //let consensus_get_request = consensus::GetRequest::new();
-        //let (_, consensus_get_response, _) = consensus_client
-        //    .get(grpc::RequestOptions::new(), consensus_get_request)
-        //    .wait()
-        //    .unwrap();
-        //assert_eq!(
-        //    consensus_get_response.get_payload(),
-        //    String::from("successor").as_bytes()
-        //);
-
-        //// Set state to a sequence of all byte values
-        //let mut scale: Vec<u8> = vec![0; 256];
-        //for i in 0..256 {
-        //    scale[i] = i as u8;
-        //}
-
-        //let mut consensus_set_request = consensus::SetRequest::new();
-        //consensus_set_request.set_payload(scale.clone());
-        //consensus_client
-        //    .set(grpc::RequestOptions::new(), consensus_set_request)
-        //    .wait()
-        //    .unwrap();
-
-        //let consensus_get_request = consensus::GetRequest::new();
-        //let (_, consensus_get_response, _) = consensus_client
-        //    .get(grpc::RequestOptions::new(), consensus_get_request)
-        //    .wait()
-        //    .unwrap();
-        //assert_eq!(consensus_get_response.get_payload(), &scale[..]);
+    fn empty() {
+        assert_eq!(8, 8)
     }
 }
---
+++ consensus/tests/integration_test.rs
@@ -0,0 +1,6 @@
+extern crate consensus;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, 4);
+}
