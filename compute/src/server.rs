5341a142db2ce0502b84b8c20aa5d93220719c6b
Jernej Kos <jernej@kos.mx>
2018-02-06 14:25:31 +0100

    Use futures in compute node gRPC server

--- compute/src/server.rs
+++ compute/src/server.rs
@@ -1,41 +1,50 @@
 use grpc;
+
 use protobuf;
 use protobuf::Message;
-use std;
 
+use thread_local::ThreadLocal;
+
+use futures::Future;
+use futures::sync::oneshot;
+
+use std;
+use std::error::Error;
 use std::fmt::Write;
 use std::sync::Mutex;
-use std::sync::mpsc::Sender;
-use std::sync::mpsc::SyncSender;
+use std::sync::mpsc::{channel, Receiver, Sender};
 
-use libcontract_common;
+use libcontract_common::api;
 use libcontract_untrusted::enclave;
 
-use generated::compute_web3::{CallContractRequest, CallContractResponse};
-use generated::compute_web3_grpc::Compute;
-use generated::consensus;
-use generated::consensus_grpc;
-use generated::consensus_grpc::Consensus;
+use super::generated::compute_web3::{CallContractRequest, CallContractResponse};
+use super::generated::compute_web3_grpc::Compute;
+use super::generated::consensus;
+use super::generated::consensus_grpc;
+use super::generated::consensus_grpc::Consensus;
+use super::instrumentation;
 
 /// This struct describes a call sent to the worker thread.
 struct QueuedRequest {
     /// This is the request from the client.
     rpc_request: CallContractRequest,
-    /// This is a channel where the worker should send the response.
-    response_sender: SyncSender<grpc::SingleResponse<CallContractResponse>>,
+    /// This is a channel where the worker should send the response. The channel is only
+    /// available until it has been used for sending a response and is None afterwards.
+    response_sender:
+        Option<oneshot::Sender<Result<CallContractResponse, Box<Error + Sync + Send + 'static>>>>,
 }
 
 /// This struct associates a response with a request.
 struct QueuedResponse<'a> {
     /// This is the request. Notably, it owns the channel where we
     /// will be sending the response.
-    queued_request: &'a QueuedRequest,
+    queued_request: &'a mut QueuedRequest,
     /// This is the response.
-    grpc_response: grpc::SingleResponse<CallContractResponse>,
+    response: Result<CallContractResponse, Box<Error + Sync + Send + 'static>>,
 }
 
 struct CachedStateInitialized {
-    encrypted_state: libcontract_common::api::CryptoSecretbox,
+    encrypted_state: api::CryptoSecretbox,
     height: u64,
 }
 
@@ -48,7 +57,7 @@ struct ComputeServerWorker {
     /// cache or state is uninitialized.
     cached_state: Option<CachedStateInitialized>,
     /// Instrumentation objects.
-    ins: super::instrumentation::WorkerMetrics,
+    ins: instrumentation::WorkerMetrics,
 }
 
 impl ComputeServerWorker {
@@ -57,7 +66,7 @@ impl ComputeServerWorker {
         ComputeServerWorker {
             contract: Self::create_contract(&contract_filename),
             cached_state: None,
-            ins: super::instrumentation::WorkerMetrics::new(),
+            ins: instrumentation::WorkerMetrics::new(),
             // TODO: Use TLS client.
             consensus: match consensus_grpc::ConsensusClient::new_plain(
                 &consensus_host,
@@ -100,16 +109,13 @@ impl ComputeServerWorker {
 
     fn call_contract_fallible(
         &self,
-        encrypted_state_opt: Option<libcontract_common::api::CryptoSecretbox>,
+        encrypted_state_opt: Option<api::CryptoSecretbox>,
         rpc_request: &CallContractRequest,
     ) -> Result<
-        (
-            Option<libcontract_common::api::CryptoSecretbox>,
-            CallContractResponse,
-        ),
-        Box<std::error::Error>,
+        (Option<api::CryptoSecretbox>, CallContractResponse),
+        Box<Error + Sync + Send + 'static>,
     > {
-        let mut enclave_request = libcontract_common::api::EnclaveRequest::new();
+        let mut enclave_request = api::EnclaveRequest::new();
         let client_request = protobuf::parse_from_bytes(rpc_request.get_payload())?;
         enclave_request.set_client_request(client_request);
         if let Some(encrypted_state) = encrypted_state_opt {
@@ -122,7 +128,7 @@ impl ComputeServerWorker {
             self.contract.call_raw(enclave_request_bytes)
         }?;
 
-        let mut enclave_response: libcontract_common::api::EnclaveResponse =
+        let mut enclave_response: api::EnclaveResponse =
             protobuf::parse_from_bytes(&enclave_response_bytes)?;
         let mut rpc_response = CallContractResponse::new();
         rpc_response.set_payload(enclave_response.get_client_response().write_to_bytes()?);
@@ -146,7 +152,7 @@ impl ComputeServerWorker {
     fn set_cached_state(
         &mut self,
         checkpoint: &consensus::Checkpoint,
-    ) -> Result<(), Box<std::error::Error>> {
+    ) -> Result<(), Box<Error + Sync + Send + 'static>> {
         self.cached_state = Some(CachedStateInitialized {
             encrypted_state: protobuf::parse_from_bytes(checkpoint.get_payload())?,
             height: checkpoint.get_height(),
@@ -157,7 +163,7 @@ impl ComputeServerWorker {
     fn advance_cached_state(
         &mut self,
         diffs: &[Vec<u8>],
-    ) -> Result<libcontract_common::api::CryptoSecretbox, Box<std::error::Error>> {
+    ) -> Result<api::CryptoSecretbox, Box<Error + Sync + Send + 'static>> {
         #[cfg(feature = "no_diffs")]
         assert!(
             diffs.is_empty(),
@@ -166,18 +172,16 @@ impl ComputeServerWorker {
 
         let csi = self.cached_state
             .as_mut()
-            .ok_or::<Box<std::error::Error>>(From::from(
+            .ok_or::<Box<Error + Sync + Send + 'static>>(From::from(
                 "advance_cached_state called with uninitialized cached state",
             ))?;
         for diff in diffs {
-            let mut res: libcontract_common::api::StateApplyResponse =
-                self.contract
-                    .call(libcontract_common::api::METHOD_STATE_APPLY, &{
-                        let mut req = libcontract_common::api::StateApplyRequest::new();
-                        req.set_old(csi.encrypted_state.clone());
-                        req.set_diff(protobuf::parse_from_bytes(diff)?);
-                        req
-                    })?;
+            let mut res: api::StateApplyResponse = self.contract.call(api::METHOD_STATE_APPLY, &{
+                let mut req = api::StateApplyRequest::new();
+                req.set_old(csi.encrypted_state.clone());
+                req.set_diff(protobuf::parse_from_bytes(diff)?);
+                req
+            })?;
             csi.encrypted_state = res.take_new();
             csi.height += 1;
         }
@@ -186,8 +190,8 @@ impl ComputeServerWorker {
 
     fn call_contract_batch_fallible<'a>(
         &mut self,
-        request_batch: &'a [QueuedRequest],
-    ) -> Result<Vec<QueuedResponse<'a>>, Box<std::error::Error>> {
+        request_batch: &'a mut [QueuedRequest],
+    ) -> Result<Vec<QueuedResponse<'a>>, Box<Error + Sync + Send + 'static>> {
         // Get state updates from consensus
         let mut encrypted_state_opt = if self.consensus.is_some() {
             let _consensus_get_timer = self.ins.consensus_get_time.start_timer();
@@ -243,9 +247,9 @@ impl ComputeServerWorker {
         // Process the requests.
         let mut ever_update_state = false;
         let response_batch = request_batch
-            .iter()
-            .map(|ref queued_request| {
-                let grpc_response = match self.call_contract_fallible(
+            .iter_mut()
+            .map(|queued_request| {
+                let response = match self.call_contract_fallible(
                     encrypted_state_opt.clone(),
                     &queued_request.rpc_request,
                 ) {
@@ -254,16 +258,18 @@ impl ComputeServerWorker {
                             encrypted_state_opt = Some(new_encrypted_state);
                             ever_update_state = true;
                         }
-                        grpc::SingleResponse::completed(rpc_response)
+
+                        Ok(rpc_response)
                     }
-                    Err(e) => {
-                        eprintln!("compute: error in call {:?}", e);
-                        grpc::SingleResponse::err(grpc::Error::Panic(String::from(e.description())))
+                    Err(error) => {
+                        eprintln!("compute: error in call {:?}", error);
+                        Err(error)
                     }
                 };
+
                 QueuedResponse {
                     queued_request,
-                    grpc_response,
+                    response,
                 }
             })
             .collect();
@@ -274,9 +280,9 @@ impl ComputeServerWorker {
                 let _consensus_set_timer = self.ins.consensus_set_time.start_timer();
                 match orig_encrypted_state_opt {
                     Some(orig_encrypted_state) => {
-                        let diff_res: libcontract_common::api::StateDiffResponse =
-                            self.contract.call(libcontract_common::api::METHOD_STATE_DIFF, &{
-                                let mut diff_req = libcontract_common::api::StateDiffRequest::new();
+                        let diff_res: api::StateDiffResponse =
+                            self.contract.call(api::METHOD_STATE_DIFF, &{
+                                let mut diff_req = api::StateDiffRequest::new();
                                 diff_req.set_old(orig_encrypted_state);
                                 diff_req.set_new(encrypted_state);
                                 diff_req
@@ -307,42 +313,60 @@ impl ComputeServerWorker {
         Ok(response_batch)
     }
 
-    fn call_contract_batch(&mut self, request_batch: Vec<QueuedRequest>) {
-        match self.call_contract_batch_fallible(&request_batch) {
-            Ok(response_batch) => {
-                // No batch-wide errors. Send out per-call responses.
-                for queued_response in response_batch {
-                    queued_response
-                        .queued_request
-                        .response_sender
-                        .send(queued_response.grpc_response)
-                        .unwrap();
+    fn call_contract_batch(&mut self, mut request_batch: Vec<QueuedRequest>) {
+        // Contains a batch-wide error if one has occurred.
+        let batch_error: Option<String>;
+
+        {
+            match self.call_contract_batch_fallible(&mut request_batch) {
+                Ok(response_batch) => {
+                    // No batch-wide errors. Send out per-call responses.
+                    for queued_response in response_batch {
+                        let sender = queued_response
+                            .queued_request
+                            .response_sender
+                            .take()
+                            .unwrap();
+                        sender.send(queued_response.response).unwrap();
+                    }
+
+                    return;
+                }
+                Err(error) => {
+                    // Batch-wide error has occurred. We cannot handle the error here as we
+                    // must first drop the mutable request_batch reference.
+                    eprintln!("compute: batch-wide error {:?}", error);
+
+                    batch_error = Some(String::from(error.description()));
                 }
             }
-            Err(e) => {
-                eprintln!("compute: batch-wide error {:?}", e);
-                // Send batch-wide error to all clients.
-                let desc = String::from(e.description());
-                for queued_request in &request_batch {
-                    let grpc_response = grpc::SingleResponse::err(grpc::Error::Panic(desc.clone()));
-                    queued_request.response_sender.send(grpc_response).unwrap();
-                }
-            }
+        }
+
+        // Send batch-wide error to all clients.
+        let batch_error = batch_error.as_ref().unwrap();
+        for mut queued_request in request_batch {
+            let sender = queued_request.response_sender.take().unwrap();
+            sender
+                .send(Err(Box::new(grpc::Error::Panic(batch_error.clone()))))
+                .unwrap();
         }
     }
 
     /// Process requests from a receiver until the channel closes.
-    fn work(&mut self, request_receiver: std::sync::mpsc::Receiver<QueuedRequest>) {
+    fn work(&mut self, request_receiver: Receiver<QueuedRequest>) {
         // Block for the next call.
         while let Ok(queued_request) = request_receiver.recv() {
             self.ins.reqs_batches_started.inc();
             let _batch_timer = self.ins.req_time_batch.start_timer();
+
             let mut request_batch = Vec::new();
             request_batch.push(queued_request);
+
             // Additionally dequeue any remaining requests.
             while let Ok(queued_request) = request_receiver.try_recv() {
                 request_batch.push(queued_request);
             }
+
             // Process the requests.
             self.call_contract_batch(request_batch);
         }
@@ -350,10 +374,14 @@ impl ComputeServerWorker {
 }
 
 pub struct ComputeServerImpl {
-    /// Channel for submitting requests to the worker.
+    /// Channel for submitting requests to the worker. This is only used to
+    /// initialize a thread-local clone of the sender handle, so that there
+    /// is no need for locking during request processing.
     request_sender: Mutex<Sender<QueuedRequest>>,
+    /// Thread-local channel for submitting requests to the worker.
+    tl_request_sender: ThreadLocal<Sender<QueuedRequest>>,
     /// Instrumentation objects.
-    ins: super::instrumentation::HandlerMetrics,
+    ins: instrumentation::HandlerMetrics,
 }
 
 impl ComputeServerImpl {
@@ -361,7 +389,8 @@ impl ComputeServerImpl {
     pub fn new(contract_filename: &str, consensus_host: &str, consensus_port: u16) -> Self {
         let contract_filename_owned = String::from(contract_filename);
         let consensus_host_owned = String::from(consensus_host);
-        let (request_sender, request_receiver) = std::sync::mpsc::channel();
+
+        let (request_sender, request_receiver) = channel();
         // move request_receiver
         std::thread::spawn(move || {
             ComputeServerWorker::new(
@@ -370,10 +399,21 @@ impl ComputeServerImpl {
                 consensus_port,
             ).work(request_receiver);
         });
+
         ComputeServerImpl {
             request_sender: Mutex::new(request_sender),
-            ins: super::instrumentation::HandlerMetrics::new(),
+            tl_request_sender: ThreadLocal::new(),
+            ins: instrumentation::HandlerMetrics::new(),
         }
+    }
+
+    /// Get thread-local request sender.
+    fn get_request_sender(&self) -> &Sender<QueuedRequest> {
+        self.tl_request_sender.get_or(|| {
+            // Only take the lock when we need to clone the sender for a new thread.
+            let request_sender = self.request_sender.lock().unwrap();
+            Box::new(request_sender.clone())
+        })
     }
 }
 
@@ -383,18 +423,24 @@ impl Compute for ComputeServerImpl {
         _options: grpc::RequestOptions,
         rpc_request: CallContractRequest,
     ) -> grpc::SingleResponse<CallContractResponse> {
+        // Instrumentation.
         self.ins.reqs_received.inc();
         let _client_timer = self.ins.req_time_client.start_timer();
-        let (response_sender, response_receiver) = std::sync::mpsc::sync_channel(0);
-        {
-            let request_sender = self.request_sender.lock().unwrap();
-            request_sender
-                .send(QueuedRequest {
-                    rpc_request,
-                    response_sender,
-                })
-                .unwrap();
-        }
-        response_receiver.recv().unwrap()
+
+        // Send request to worker thread.
+        let (response_sender, response_receiver) = oneshot::channel();
+        self.get_request_sender()
+            .send(QueuedRequest {
+                rpc_request,
+                response_sender: Some(response_sender),
+            })
+            .unwrap();
+
+        // Prepare response future.
+        grpc::SingleResponse::no_metadata(response_receiver.then(|result| match result {
+            Ok(Ok(response)) => Ok(response),
+            Ok(Err(error)) => Err(grpc::Error::Panic(error.description().to_string())),
+            Err(error) => Err(grpc::Error::Panic(error.description().to_string())),
+        }))
     }
 }
