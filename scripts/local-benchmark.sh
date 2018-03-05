a0cc0360c90e12956d8ba836c8a5fa9f9bf3803a
w <none>
2018-03-05 14:51:43 -0800

    remove unused script

--- scripts/local-benchmark.sh
+++
@@ -1,3 +0,0 @@
-./target/debug/consensus -x &
-./target/debug/compute --disable-key-manager --port 9003 ./target/enclave/key-manager.signed.so &
-./target/debug/compute ./target/enclave/token.signed.so &
