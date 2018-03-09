0a9e87a77be6230f494c29cda1bcd7703d10f1bf
Jernej Kos <jernej@kos.mx>
2018-03-09 13:17:31 +0100

    Update token contract to use the new db interface

--- contracts/token/src/lib.rs
+++ contracts/token/src/lib.rs
@@ -10,13 +10,15 @@ extern crate token_api;
 
 mod token_contract;
 
+use protobuf::Message;
+
 use token_api::{with_api, CreateRequest, CreateResponse, GetBalanceRequest, GetBalanceResponse,
                 TransferRequest, TransferResponse};
 use token_contract::TokenContract;
 
 use ekiden_core_common::Result;
 use ekiden_core_common::contract::{with_contract_state, Address, Contract};
-use ekiden_core_trusted::db::Db;
+use ekiden_core_trusted::db::{Database, DatabaseHandle};
 use ekiden_core_trusted::rpc::create_enclave_rpc;
 
 // Create enclave RPC handlers.
@@ -25,6 +27,7 @@ with_api! {
 }
 
 fn create(request: &CreateRequest) -> Result<CreateResponse> {
+    let mut db = DatabaseHandle::instance();
     let contract = TokenContract::new(
         &Address::from(request.get_sender().to_string()),
         request.get_initial_supply(),
@@ -32,13 +35,15 @@ fn create(request: &CreateRequest) -> Result<CreateResponse> {
         request.get_token_symbol().to_string(),
     );
 
-    Db::instance().set("state", contract.get_state())?;
+    db.insert(b"state", &contract.get_state().write_to_bytes()?);
 
     Ok(CreateResponse::new())
 }
 
 fn transfer(request: &TransferRequest) -> Result<TransferResponse> {
-    let state = Db::instance().get("state")?;
+    let mut db = DatabaseHandle::instance();
+    let state = protobuf::parse_from_bytes(&db.get(b"state").unwrap())?;
+
     let state = with_contract_state(&state, |contract: &mut TokenContract| {
         contract.transfer(
             &Address::from(request.get_sender().to_string()),
@@ -49,13 +54,15 @@ fn transfer(request: &TransferRequest) -> Result<TransferResponse> {
         Ok(())
     })?;
 
-    Db::instance().set("state", state)?;
+    db.insert(b"state", &state.write_to_bytes()?);
 
     Ok(TransferResponse::new())
 }
 
 fn get_balance(request: &GetBalanceRequest) -> Result<GetBalanceResponse> {
-    let contract = TokenContract::from_state(&Db::instance().get("state")?);
+    let db = DatabaseHandle::instance();
+    let contract =
+        TokenContract::from_state(&protobuf::parse_from_bytes(&db.get(b"state").unwrap())?);
     let balance = contract.get_balance(&Address::from(request.get_account().to_string()))?;
 
     let mut response = GetBalanceResponse::new();
