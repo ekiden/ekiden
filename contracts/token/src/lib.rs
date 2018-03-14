2bfe7eaf7394d284bbac93af05fa79396da9bd19
Jernej Kos <jernej@kos.mx>
2018-03-14 14:01:09 +0100

    Make token contract use the schema-based interface

--- contracts/token/src/lib.rs
+++ contracts/token/src/lib.rs
@@ -5,20 +5,15 @@ extern crate protobuf;
 extern crate ekiden_core_common;
 extern crate ekiden_core_trusted;
 
-#[macro_use]
 extern crate token_api;
 
 mod token_contract;
-
-use protobuf::Message;
 
 use token_api::{with_api, CreateRequest, CreateResponse, GetBalanceRequest, GetBalanceResponse,
                 TransferRequest, TransferResponse};
 use token_contract::TokenContract;
 
 use ekiden_core_common::Result;
-use ekiden_core_common::contract::{with_contract_state, Address, Contract};
-use ekiden_core_trusted::db::{Database, DatabaseHandle};
 use ekiden_core_trusted::rpc::create_enclave_rpc;
 
 // Create enclave RPC handlers.
@@ -27,43 +22,37 @@ with_api! {
 }
 
 fn create(request: &CreateRequest) -> Result<CreateResponse> {
-    let mut db = DatabaseHandle::instance();
-    let contract = TokenContract::new(
-        &Address::from(request.get_sender().to_string()),
-        request.get_initial_supply(),
-        request.get_token_name().to_string(),
-        request.get_token_symbol().to_string(),
-    );
+    let token = TokenContract::new();
 
-    db.insert(b"state", &contract.get_state().write_to_bytes()?);
+    // TODO: Get sender from authenticated request.
+    token.create(
+        request.get_sender().to_owned(),
+        request.get_token_name().to_owned(),
+        request.get_token_symbol().to_owned(),
+        request.get_initial_supply(),
+    )?;
 
     Ok(CreateResponse::new())
 }
 
 fn transfer(request: &TransferRequest) -> Result<TransferResponse> {
-    let mut db = DatabaseHandle::instance();
-    let state = protobuf::parse_from_bytes(&db.get(b"state").unwrap())?;
+    let token = TokenContract::new();
 
-    let state = with_contract_state(&state, |contract: &mut TokenContract| {
-        contract.transfer(
-            &Address::from(request.get_sender().to_string()),
-            &Address::from(request.get_destination().to_string()),
-            request.get_value(),
-        )?;
-
-        Ok(())
-    })?;
-
-    db.insert(b"state", &state.write_to_bytes()?);
+    // TODO: Get sender from authenticated request.
+    token.transfer(
+        request.get_sender().to_owned(),
+        request.get_destination().to_owned(),
+        request.get_value(),
+    )?;
 
     Ok(TransferResponse::new())
 }
 
 fn get_balance(request: &GetBalanceRequest) -> Result<GetBalanceResponse> {
-    let db = DatabaseHandle::instance();
-    let contract =
-        TokenContract::from_state(&protobuf::parse_from_bytes(&db.get(b"state").unwrap())?);
-    let balance = contract.get_balance(&Address::from(request.get_account().to_string()))?;
+    let token = TokenContract::new();
+
+    // TODO: Get sender from authenticated request.
+    let balance = token.get_balance(&request.get_account().to_owned())?;
 
     let mut response = GetBalanceResponse::new();
     response.set_balance(balance);
--- contracts/token/src/token_contract.rs
+++ contracts/token/src/token_contract.rs
@@ -1,132 +1,118 @@
-// Inspired by https://www.ethereum.org/token
-
-use std::collections::HashMap;
-
 use ekiden_core_common::{Error, Result};
-use ekiden_core_common::contract::{Address, Contract};
 
-use token_api::TokenState;
+use ekiden_core_trusted::db::database_schema;
+
+database_schema! {
+    pub struct TokenDb {
+        pub name: String,
+        pub symbol: String,
+        pub total_supply: u64,
+        pub balance_of: Map<String, u64>,
+    }
+}
 
 pub struct TokenContract {
-    name: String,
-    symbol: String,
-    total_supply: u64,
-    balance_of: HashMap<String, u64>,
+    /// Token database.
+    db: TokenDb,
 }
 
 impl TokenContract {
-    pub fn new(
-        msg_sender: &Address,
+    pub fn new() -> Self {
+        TokenContract { db: TokenDb::new() }
+    }
+
+    pub fn create(
+        &self,
+        sender: String,
+        name: String,
+        symbol: String,
         initial_supply: u64,
-        token_name: String,
-        token_symbol: String,
-    ) -> TokenContract {
+    ) -> Result<()> {
+        // TODO: Ensure that the contract has not yet been initialized.
+
         let decimals = 18;
         let total_supply = initial_supply * 10u64.pow(decimals);
 
-        TokenContract {
-            name: token_name.clone(),
-            symbol: token_symbol.clone(),
-            total_supply: total_supply,
-            balance_of: {
-                let mut h = HashMap::new();
-                h.insert(msg_sender.to_string(), total_supply);
-                h
-            },
-        }
+        // Initialize contract, overwriting any previous state.
+        self.db.name.insert(&name);
+        self.db.symbol.insert(&symbol);
+        self.db.total_supply.insert(&total_supply);
+        self.db.balance_of.insert(&sender, &total_supply);
+
+        Ok(())
     }
 
-    // PRIVATE METHODS
-    fn get_from_balance(&self, addr: &Address, value: u64) -> Result<u64> {
-        match self.balance_of.get(addr.as_str()) {
+    fn get_from_balance(&self, addr: &String, value: u64) -> Result<u64> {
+        match self.db.balance_of.get(addr) {
             None => Err(Error::new("Nonexistent `from` account")),
-            Some(b) if *b < value => Err(Error::new("Insufficient `from` balance")),
-            Some(b) => Ok(*b),
+            Some(b) if b < value => Err(Error::new("Insufficient `from` balance")),
+            Some(b) => Ok(b),
         }
     }
 
-    fn get_to_balance(&self, addr: &Address) -> Result<u64> {
-        match self.balance_of.get(addr.as_str()) {
-            Some(b) => Ok(*b),
+    fn get_to_balance(&self, addr: &String) -> Result<u64> {
+        match self.db.balance_of.get(addr) {
+            Some(b) => Ok(b),
             None => Ok(0),
         }
     }
 
-    fn do_transfer(&mut self, from: &Address, to: &Address, value: u64) -> Result<()> {
-        let from_balance = self.get_from_balance(from, value)?;
-        let to_balance = self.get_to_balance(to)?;
+    fn do_transfer(&self, from: String, to: String, value: u64) -> Result<()> {
+        let from_balance = self.get_from_balance(&from, value)?;
+        let to_balance = self.get_to_balance(&to)?;
         if to_balance + value <= to_balance {
             return Err(Error::new(
                 "Transfer value too large, overflow `to` account",
             ));
         }
 
-        // Set new balances
+        // Set new balances.
         let previous_balances = from_balance + to_balance;
         let from_balance = from_balance - value;
         let to_balance = to_balance + value;
-        self.balance_of.insert(from.to_string(), from_balance);
-        self.balance_of.insert(to.to_string(), to_balance);
+        self.db.balance_of.insert(&from, &from_balance);
+        self.db.balance_of.insert(&to, &to_balance);
 
-        //Emit Transfer(_from, _to, _value) event;
-        assert_eq!(
-            previous_balances,
-            (self.balance_of.get(from.as_str()).unwrap()
-                + self.balance_of.get(to.as_str()).unwrap()),
-            "new balance sum should equal old balance sum after transfer"
-        );
-
-        return Ok(());
+        Ok(())
     }
 
     // PUBLIC METHODS
     // - callable over RPC
     pub fn get_name(&self) -> Result<String> {
-        Ok(self.name.clone())
+        match self.db.name.get() {
+            Some(name) => Ok(name),
+            None => Err(Error::new("Contract not yet initialized")),
+        }
     }
 
     pub fn get_symbol(&self) -> Result<String> {
-        Ok(self.symbol.clone())
+        match self.db.symbol.get() {
+            Some(symbol) => Ok(symbol),
+            None => Err(Error::new("Contract not yet initialized")),
+        }
     }
 
-    pub fn get_balance(&self, msg_sender: &Address) -> Result<u64> {
+    pub fn get_balance(&self, msg_sender: &String) -> Result<u64> {
         self.get_to_balance(msg_sender)
     }
 
-    pub fn transfer(&mut self, msg_sender: &Address, to: &Address, value: u64) -> Result<()> {
+    pub fn transfer(&self, msg_sender: String, to: String, value: u64) -> Result<()> {
         self.do_transfer(msg_sender, to, value)
     }
 
-    pub fn burn(&mut self, msg_sender: &Address, value: u64) -> Result<()> {
-        let from_balance = self.get_from_balance(msg_sender, value)?;
-        self.balance_of
-            .insert(msg_sender.to_string(), from_balance - value);
-        self.total_supply -= value;
-        // Emit Burn(msg_sender, value) event;
+    pub fn burn(&self, msg_sender: String, value: u64) -> Result<()> {
+        let total_supply = match self.db.total_supply.get() {
+            Some(supply) => supply,
+            None => return Err(Error::new("Contract not yet initialized")),
+        };
+
+        let from_balance = self.get_from_balance(&msg_sender, value)?;
+        self.db
+            .balance_of
+            .insert(&msg_sender, &(from_balance - value));
+        self.db.total_supply.insert(&(total_supply - value));
+
         Ok(())
-    }
-}
-
-impl Contract<TokenState> for TokenContract {
-    /// Get serializable contract state.
-    fn get_state(&self) -> TokenState {
-        let mut state = TokenState::new();
-        state.set_name(self.name.clone());
-        state.set_symbol(self.symbol.clone());
-        state.set_total_supply(self.total_supply);
-        state.set_balance_of(self.balance_of.clone());
-
-        state
-    }
-
-    /// Create contract instance from serialized state.
-    fn from_state(state: &TokenState) -> TokenContract {
-        TokenContract {
-            name: state.get_name().to_string(),
-            symbol: state.get_symbol().to_string(),
-            total_supply: state.get_total_supply(),
-            balance_of: state.get_balance_of().clone(),
-        }
     }
 }
 
@@ -135,24 +121,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new_contract() {
+    fn test_contract() {
         let name = "Ekiden Token";
         let symbol = "EKI";
-        let a1 = Address::from(String::from("testaddr"));
-        let c = TokenContract::new(&a1, 8, String::from(name), String::from(symbol));
-        assert_eq!(name, c.get_name().unwrap(), "name should be set");
-        assert_eq!(symbol, c.get_symbol().unwrap(), "symbol should be set");
-        assert!(0 < c.total_supply, "total_supply should be set");
+        let a1 = "testaddr";
+
+        let contract = TokenContract::new();
+        contract.create(
+            "testaddr".to_owned(),
+            "Ekiden Token".to_owned(),
+            "EKI".to_owned(),
+            8,
+        );
+
+        assert_eq!(name, contract.get_name().unwrap(), "name should be set");
+        assert_eq!(
+            symbol,
+            contract.get_symbol().unwrap(),
+            "symbol should be set"
+        );
+
+        assert_eq!(
+            contract.get_balance(&"testaddr".to_owned()).unwrap(),
+            8_000_000_000_000_000_000,
+            "creator should get all the tokens"
+        );
     }
-
-    #[test]
-    fn get_initial_balance() {
-        let a1 = Address::from(String::from("testaddr"));
-        let c = TokenContract::new(&a1, 8, String::from("Ekiden Tokiden"), String::from("EKI"));
-        let b = c.get_balance(&a1).expect("testaddr should have tokens");
-        assert_eq!(c.total_supply, b, "creator should get all the tokens");
-    }
-
-    // @todo - add more tests
-
 }
