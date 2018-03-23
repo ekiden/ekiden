068e398906bc2eda56b2f17aca4b5054987b3bbe
Jernej Kos <jernej@kos.mx>
2018-03-23 13:32:49 +0100

    Use .init_array initializer for RPC registrations

--- rpc/trusted/src/macros.rs
+++ rpc/trusted/src/macros.rs
@@ -22,33 +22,30 @@ macro_rules! create_enclave_rpc {
             rpc $method_name:ident ( $request_type:ty ) -> $response_type:ty ;
         )*
     ) => {
-        /// Generate method that will register all RPC methods.
-        ///
-        /// This method will be automatically invoked when `rpc_init` ECALL is invoked
-        /// from the outside.
-        #[no_mangle]
-        #[doc(hidden)]
-        pub extern "C" fn __ekiden_rpc_create_enclave() {
-            use ekiden_core::error::Result;
-            use ekiden_core::rpc::reflection::ApiMethodDescriptor;
-            use ekiden_trusted::rpc::dispatcher::{Dispatcher, EnclaveMethod};
-            use ekiden_trusted::rpc::request::Request;
+        #[cfg(target_env = "sgx")]
+        global_ctors_object! {
+            ENCLAVE_RPC_INIT, enclave_rpc_init = {
+                use ekiden_core::error::Result;
+                use ekiden_core::rpc::reflection::ApiMethodDescriptor;
+                use ekiden_trusted::rpc::dispatcher::{Dispatcher, EnclaveMethod};
+                use ekiden_trusted::rpc::request::Request;
 
-            // Register generated methods using the dispatcher.
-            let mut dispatcher = Dispatcher::get();
-            $(
-                dispatcher.add_method(
-                    EnclaveMethod::new(
-                        ApiMethodDescriptor {
-                            name: stringify!($method_name).to_owned(),
-                            client_attestation_required: $client_attestation_required,
-                        },
-                        |request: &Request<$request_type>| -> Result<$response_type> {
-                            $method_name(request)
-                        },
-                    )
-                );
-            )*
+                // Register generated methods using the dispatcher.
+                let mut dispatcher = Dispatcher::get();
+                $(
+                    dispatcher.add_method(
+                        EnclaveMethod::new(
+                            ApiMethodDescriptor {
+                                name: stringify!($method_name).to_owned(),
+                                client_attestation_required: $client_attestation_required,
+                            },
+                            |request: &Request<$request_type>| -> Result<$response_type> {
+                                $method_name(request)
+                            },
+                        )
+                    );
+                )*
+            }
         }
     }
 }
